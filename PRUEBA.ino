2d783643571e9a59270b582a08f227727556d098
davidotero46 <119788916+davidotero46@users.noreply.github.com>
2023-03-13 12:55:16 +0100

    Update PRUEBA.ino

--- PRUEBA.ino
+++ PRUEBA.ino
@@ -5,7 +5,6 @@
     Autor: DAVID ANDRES OTERO ELIA.
     Data: febrero del 2023
 */
-
 #include <Servo.h>
 
 //Pin de control do servo
@@ -14,65 +13,24 @@
 //Declaramos o obxecto motor
 //da clase Servo
 Servo motor;
-int veloc = 20;
-
-String orde ="";
-int posicion = 0;
+int veloc = 100;
 
 void setup() {
   motor.attach(CTRL);
-  Serial.begin(9600);
 }
 
 void loop() {
-  //Comprobamos se hai orde no teclado
-  if(Serial.available()) {
-    orde = Serial.readStringUntil('\n');
-    orde.toLowerCase();
-    if(orde.equals("esquerda")) posicion = 180;
-    else if(orde.equals("dereita")) posicion = 0;
-    else if(orde.equals("centro")) posicion = 90;
-    else {
-      int tmp = orde.toInt();
-      if(tmp >= 0 && tmp <= 180) posicion = tmp;
-      else posicion = 0;      
-    }
+  //Move o servo entre 0º e 180º en
+  //incrementos de 5º
+  for(int i = 0; i <= 180; i++) {
+    motor.write(i);
+    delay(veloc);
   }
-  motor.write(posicion);
-  delay(veloc);
-}
-#include <Servo.h>
-
-//Pin de control do servo
-#define CTRL 9
-
-//Declaramos o obxecto motor
-//da clase Servo
-Servo motor;
-int veloc = 20;
-
-String orde ="";
-int posicion = 0;
-
-void setup() {
-  motor.attach(CTRL);
-  Serial.begin(9600);
-}
-
-void loop() {
-  //Comprobamos se hai orde no teclado
-  if(Serial.available()) {
-    orde = Serial.readStringUntil('\n');
-    orde.toLowerCase();
-    if(orde.equals("esquerda")) posicion = 180;
-    else if(orde.equals("dereita")) posicion = 0;
-    else if(orde.equals("centro")) posicion = 90;
-    else {
-      int tmp = orde.toInt();
-      if(tmp >= 0 && tmp <= 180) posicion = tmp;
-      else posicion = 0;      
-    }
+  delay(15*veloc);
+  //Move o servo entre 180º e 0º en
+  //decrementos de 5º
+  for(int i = 180; i >= 0; i--) {
+    motor.write(i);
+    delay(veloc);
   }
-  motor.write(posicion);
-  delay(veloc);
 }
