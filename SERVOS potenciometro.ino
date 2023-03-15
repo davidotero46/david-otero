4be32fb4060f213d1d87bd82e24794a40683be54
davidotero46 <119788916+davidotero46@users.noreply.github.com>
2023-03-15 16:40:58 +0100

    Update SERVOS potenciometro.ino

--- SERVOS potenciometro.ino
+++ SERVOS potenciometro.ino
@@ -1,4 +1,12 @@
 /*
+    servo motor con potenciometro.
+    unha montaxe na que un servo (alimentado por unha batería externa) se conecte no pin PWM D03 (3~) como entrada de control (saída para Arduíno)
+    e un potenciómetro se conecte no pin analóxico A05 como entrada do Arduíno.
+    A continuación realiza un programa en C++ de Arduíno, de maneira que o potenciómetro controle o ángulo de xiro do servo,
+    supoñendo que este pode xirar un máximo de 180º.
+    
+    nombre: david andres otero elia
+    data: 15/03/2023
  * Proba de funcionamento dun servomotor
  * empregando a librería <Servo.h> e 
  * ligado a un potenciómetro.
@@ -9,10 +17,10 @@
 //Pin de control do servo
 #define CTRL 9
 
-//Pin de lectura do potenc
+//Pin de lectura do potenciometro
 #define POT A0
 
-//Declaramos o obxecto motor
+//Declaramos o objeto motor
 //da clase Servo
 Servo motor;
 int angulo = 0;
